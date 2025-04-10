74d0d7c1d7da8a5254ce39f2e6465bbecaf42d89
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-04-10 18:39:56 -0400

    test: added test should_insert_at_index for insert_at_index

--- src/test.rs
+++ src/test.rs
@@ -12,4 +12,16 @@ mod test {
         assert_eq!(list.get(1), Some(2));
         assert_eq!(list.get(2), Some(3));
     }
+
+    #[test]
+    fn should_insert_at_index() {
+        let mut list = DynamicLinkedList::new();
+        list.insert(1);
+        list.insert(3);
+        list.insert_at_index(1, 2);
+
+        assert_eq!(list.get(0), Some(1));
+        assert_eq!(list.get(1), Some(2));
+        assert_eq!(list.get(2), Some(3));
+    }
 }
