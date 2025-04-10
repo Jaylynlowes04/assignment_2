259715f902ec59f86fa2885c10d157f8c25d0057
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-04-10 18:27:30 -0400

    test: added test should_insert_and_get for insert and get

---
+++ src/test.rs
@@ -0,0 +1,15 @@
+mod test {
+    use crate::DynamicLinkedList;
+
+
+    #[test]
+    fn should_insert_and_get() {
+        let mut list = DynamicLinkedList::new();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        assert_eq!(list.get(0), Some(1));
+        assert_eq!(list.get(1), Some(2));
+        assert_eq!(list.get(2), Some(3));
+    }
+}
