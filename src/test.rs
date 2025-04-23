a0b5d73568a2c4e98c88ba9a4d0ef263e7cb6fbd
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-04-22 21:22:03 -0400

    test: added test for deletion at a certain index

--- src/test.rs
+++ src/test.rs
@@ -37,4 +37,16 @@ mod test {
         assert_eq!(list.get(1), Some(3));
         assert!(!list.delete_element(99));
     }
+
+    #[test]
+    fn should_delete_at_index() {
+        let mut list = DynamicLinkedList::new();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        assert!(list.delete_at_index(1));
+        assert_eq!(list.get(0), Some(1));
+        assert_eq!(list.get(1), Some(3));
+        assert!(!list.delete_at_index(5));
+    }
 }
